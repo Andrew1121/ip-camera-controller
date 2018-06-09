244876af47277fba02fdfb4f4a77f0bc92f316a1
DESKTOP-96TNPVQ\andrew <11panaga21@gmail.com>
2018-06-10 00:01:17 +0800

    [FYP] basic file setup

---
+++ js/all.js
@@ -0,0 +1,15 @@
+$(function(){
+
+  // $('#video-stream').networkCamera({
+  //   url: 'http://192.168.1.107/cgi-bin/api.cgi?cmd=Snap&channel=0&user=admin&password=a1121@C1123',
+  // });
+  // $('#video-stream').networkCamera('stream');
+  // setInterval(function(){
+  //   var url = "http://192.168.1.107/cgi-bin/api.cgi?cmd=Snap&channel=0&user=admin&password=a1121@C1123";
+  //   $('#video-stream').css('background-image', 'url(' + url + ')');
+  // },1000);
+  // setInterval(function(){
+  //   $('#video-stream').css('background-image', '');
+  // },2000)
+
+});
